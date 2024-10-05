8d053b622008be9905ca25383beeb4da2622e0f0
Ankit Kumar <ankitchauhandhoni@gmail.com>
2024-10-05 21:25:39 +0530

    g

--- backend/Utils/token.js
+++ backend/Utils/token.js
@@ -1,22 +1,22 @@
-    const jwt = require('jsonwebtoken');
-    const dotenv=require('dotenv');
-    dotenv.config();
-     
-    const generateTokenandCookie = (userID, res) => {
-        const token = jwt.sign({ userID }, process.env.JWT_SECRET, { expiresIn: "15d" });
-         console.log("token is generated:",token);
-         
-       console.log(process.env.JWT_SECRET);
+        const jwt = require('jsonwebtoken');
+        const dotenv=require('dotenv');
+        dotenv.config();
+        
+        const generateTokenandCookie = (userID, res) => {
+            const token = jwt.sign({ userID }, process.env.JWT_SECRET, { expiresIn: "15d" });
+            console.log("token is generated:",token);
+            
+        console.log(process.env.JWT_SECRET);
 
-        res.cookie("jwt-imdb", token, {
-            maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-            httpOnly: true, 
-            sameSite: "None", 
-            secure: true,
-            path:'/'
-        });
+            res.cookie("jwt-imdb", token, {
+                maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+                httpOnly: true, 
+                sameSite: "None", 
+                secure: true,
+                path:'/'
+            });
 
-        return token;
-    }
+            return token;
+        }
 
-    module.exports = generateTokenandCookie;
+        module.exports = generateTokenandCookie;
