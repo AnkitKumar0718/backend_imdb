3d42123c56423bace6126ae624d9b72a5a0bd781
Ankit Kumar <ankitchauhandhoni@gmail.com>
2024-10-05 21:30:32 +0530

    h

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://imdb-movie-fullstack.netlify.app'], // Array of allowed origins
+    origin: ['http://localhost:3000', 'https://imdb-movie-clone7788.netlify.app'], // Array of allowed origins
     credentials: true // Enable this to allow cookies to be sent
 }));            
 
